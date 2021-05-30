a6496776470f0dea5430391062a468687ca07f8d
angela-jennings <amjennings118@gmail.com>
2021-05-30 12:05:22 -0600

    finished test case for drop down

--- src/test/java/com/cybertek/tests/day7_alerts_iframes_windows/DropdownPractice.java
+++ src/test/java/com/cybertek/tests/day7_alerts_iframes_windows/DropdownPractice.java
@@ -49,8 +49,40 @@ public class DropdownPractice {
 
         }
 
+
+    @Test
+    public void tc2_state_dropdown_test(){
+
+        //LOCATING STATE DROPDOWN
+        Select stateDropdown = new Select(driver.findElement(By.xpath("//select[@id='state']")));
+
+        //3. Select Illinois
+        stateDropdown.selectByVisibleText("Illinois");
+        //using the different ways to select in a dropdown
+
+        //4. Select Virginia
+        stateDropdown.selectByValue("VA");
+        //this and by visible text are probably the most straightforward
+
+        //5. Select California
+        stateDropdown.selectByIndex(5);
+        //generally least efficient because you have to count. California is just near the top, but obviously wouldnt use for virginia or other states farther down.
+
+        //6. Verify final selected option is California.
+        String expOptionText = "California";
+        String actualOptionText = stateDropdown.getFirstSelectedOption().getText();
+        Assert.assertEquals(actualOptionText, expOptionText, "this message will be printed out only if assertion fails");
+        //can pass a third string which will only print if test fails. not mandatory but can be useful
+
+        //Assert.assertTrue(stateDropdown.getFirstSelectedOption().getText().equals("California"));
+        //Use all Select options. (visible text, value, index)
+
+
+    }
+
         @AfterClass
-    public void tearDownClass(){
+    public void tearDownClass() throws InterruptedException {
+        Thread.sleep(5000);
         driver.close();
         }
 
