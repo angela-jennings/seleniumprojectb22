b3788f453b3197b132dc769dcab91db1c3569bb0
angela-jennings <amjennings118@gmail.com>
2021-06-10 21:23:16 -0600

    added Smart Bear tasks with utility package

---
+++ src/test/java/com/cybertek/tests/PracticeAutomation/SmartBearTasks.java
@@ -0,0 +1,68 @@
+package com.cybertek.tests.PracticeAutomation;
+
+import com.cybertek.utilities.ConfigurationReader;
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class SmartBearTasks {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setup(){
+        String browser = ConfigurationReader.getProperty("browser");
+        String url = ConfigurationReader.getProperty("dataTablesURL");
+        driver = WebDriverFactory.getDriver(browser);
+        driver.manage().timeouts().implicitlyWait(15,TimeUnit.SECONDS);
+        driver.get(url);
+    }
+
+    @Test
+    public void testCase1(){
+        SmartBearUtils.smartBearLogin(driver);
+        List<WebElement> allLinks = driver.findElements(By.xpath("//a"));
+        System.out.println("Number of links on page - " + allLinks.size());
+        for(WebElement eachLink : allLinks) {
+            System.out.println(eachLink.getText());
+        }
+    }
+
+    @Test
+    public void testCase2(){
+        SmartBearUtils.smartBearLogin(driver);
+        SmartBearUtils.addItem(driver, "FamilyAlbum", "2");
+        SmartBearUtils.inputField(driver);
+        SmartBearUtils.paymentInformation(driver);
+        Assert.assertEquals(driver.findElement(By.ByXPath.xpath("//strong")).getText(), "New order has been successfully added.");
+    }
+
+    @Test
+    public void testCase3(){
+        SmartBearUtils.smartBearLogin(driver);
+        SmartBearUtils.customerOrderDate(driver, "Susan McLaren", "01/05/2010");
+    }
+
+    @Test
+    public void testCase4(){
+        SmartBearUtils.smartBearLogin(driver);
+        SmartBearUtils.customerNameVerification(driver, "Susan McLaren");
+    }
+
+    @Test
+    public void testCase5(){
+        SmartBearUtils.smartBearLogin(driver);
+        SmartBearUtils.printNamesAndCities(driver);
+    }
+    @AfterMethod
+    public void tearDown(){
+        driver.close();
+    }
+}
---
+++ src/test/java/com/cybertek/tests/PracticeAutomation/SmartBearUtils.java
@@ -0,0 +1,82 @@
+package com.cybertek.tests.PracticeAutomation;
+
+import com.cybertek.utilities.BrowserUtils;
+import com.github.javafaker.Faker;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+
+import java.util.List;
+
+public class SmartBearUtils {
+    public static void smartBearLogin(WebDriver driver){
+        driver.findElement(By.xpath("//input[@id='ctl00_MainContent_username']")).sendKeys("Tester");
+        driver.findElement(By.xpath("//input[@id='ctl00_MainContent_password']")).sendKeys("test");
+        driver.findElement(By.xpath("//input[@id='ctl00_MainContent_login_button']")).click();
+    }
+    public static void inputField(WebDriver driver){
+        Faker faker = new Faker();
+        driver.findElement(By.xpath("//input[@id='ctl00_MainContent_fmwOrder_txtName']")).sendKeys(faker.harryPotter().character());
+        driver.findElement(By.xpath("//input[@id='ctl00_MainContent_fmwOrder_TextBox2']")).sendKeys(faker.address().streetAddress());
+        driver.findElement(By.xpath("//input[@id='ctl00_MainContent_fmwOrder_TextBox3']")).sendKeys(faker.address().city());
+        driver.findElement(By.xpath("//input[@id='ctl00_MainContent_fmwOrder_TextBox4']")).sendKeys(faker.address().state());
+        driver.findElement(By.xpath("//input[@id='ctl00_MainContent_fmwOrder_TextBox5']")).sendKeys("12345");
+    }
+
+    public static void addItem(WebDriver driver, String product, String itemAmount){
+        driver.findElement(By.xpath("//a[@href='Process.aspx']")).click();
+        Select productSelect = new Select(driver.findElement(By.xpath("//select[@id='ctl00_MainContent_fmwOrder_ddlProduct']")));
+        productSelect.selectByVisibleText(product);
+        WebElement quantity = driver.findElement(By.xpath("//input[@id='ctl00_MainContent_fmwOrder_txtQuantity']"));
+        quantity.sendKeys(Keys.DELETE);
+        quantity.sendKeys(itemAmount);
+        driver.findElement(By.xpath("//input[@type='submit']")).click();
+    }
+
+    public static void paymentInformation(WebDriver driver){
+        Faker faker = new Faker();
+        driver.findElement(By.xpath("(//input[@type='radio'])[1]")).click();
+        driver.findElement(By.xpath("//input[@name='ctl00$MainContent$fmwOrder$TextBox6']")).sendKeys(faker.finance().creditCard().replace("-", ""));
+        driver.findElement(By.xpath("//input[@name='ctl00$MainContent$fmwOrder$TextBox1']")).sendKeys("02/24");
+        BrowserUtils.sleep(2);
+        driver.findElement(By.xpath("//a[@id='ctl00_MainContent_fmwOrder_InsertButton']")).click();
+    }
+
+    public static void customerOrderDate(WebDriver driver, String customer, String orderDate){
+        driver.findElement(By.xpath("//a[@href='Default.aspx']")).click();
+        WebElement customerOrderDate = driver.findElement(By.xpath("//table[@id='ctl00_MainContent_orderGrid']//td[.='"+customer+"']/../td[5]"));
+        Assert.assertEquals(customerOrderDate.getText(), orderDate);
+    }
+
+    public static void customerNameVerification(WebDriver driver, String customerName){
+        driver.findElement(By.xpath("//a[@href='Default.aspx']")).click();
+        List<WebElement> firstNames = driver.findElements(By.xpath("//table[@id='ctl00_MainContent_orderGrid']//td[2]"));
+        for (WebElement each : firstNames){
+            if(each.getText().equals(customerName)){
+                Assert.assertTrue(true);
+                return;
+            }
+        }
+        Assert.assertTrue(false, "Given name does not match records");
+    }
+
+    public static void printNamesAndCities(WebDriver driver){
+        List<WebElement> firstNames = driver.findElements(By.xpath("//table[@id='ctl00_MainContent_orderGrid']//td[2]"));
+        int count;
+
+        for (WebElement each : firstNames){
+            count = 1;
+            System.out.println("Name" + count++ + ":" + each.getText());
+        }
+        System.out.println();
+
+        List<WebElement> cities = driver.findElements(By.xpath("//table[@id='ctl00_MainContent_orderGrid']//td[7]"));
+        for (WebElement each : cities){
+            count = 1;
+            System.out.println("City" + count++ + ":" + each.getText());
+        }
+    }
+}
