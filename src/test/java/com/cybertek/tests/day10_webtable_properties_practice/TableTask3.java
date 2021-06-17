068ae7f00273d615120f454dcc3663e5dfa4626d
angela-jennings <amjennings118@gmail.com>
2021-06-16 20:00:01 -0600

    created driver utility class

--- src/test/java/com/cybertek/tests/day10_webtable_properties_practice/TableTask3.java
+++ src/test/java/com/cybertek/tests/day10_webtable_properties_practice/TableTask3.java
@@ -1,35 +1,23 @@
 package com.cybertek.tests.day10_webtable_properties_practice;
 
+import com.cybertek.tests.base.TestBase;
 import com.cybertek.utilities.ConfigurationReader;
-import com.cybertek.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.util.concurrent.TimeUnit;
-
-public class TableTask3 {
+public class TableTask3 extends TestBase {
     //1. open browser and go to: http://practice.cybertekschool.com/tables#edit
     //2. locate first table and verify Tim has due amount of "$50"
     //Note: create locator for tim that will be dynamic and doesnt care which row tim is in
 
-    WebDriver driver;
 
-    @BeforeMethod
-    public void setup(){
-        String browser = ConfigurationReader.getProperty("browser");
-        String url = ConfigurationReader.getProperty("dataTablesURL");
-
-        driver = WebDriverFactory.getDriver(browser);
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-        driver.get(url);
-    }
     @Test
     public void task3ReturnTimDueAmount(){
+        String url = ConfigurationReader.getProperty("dataTablesURL");
+        driver.get(url);
+
         WebElement timDueAmount = driver.findElement(By.xpath("//table[@id='table1']//td[.='Tim']/../td[4]"));
     // //table[@id='table1']//td[.='Tim']/following-sibling::td[2]
 
